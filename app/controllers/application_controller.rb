02b4b21a09dc9c050b185ae7becb2a4655bf9465
github username <github email address>
2019-01-09 22:27:38 +0000

    learn submit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    # @recipe = Recipe.create(params[:recipe]) (The better way that Learn will not allow)
+    name = params[:name]
+    ingredients = params[:ingredients]
+    cook_time = params[:cook_time]
+    recipe = Recipe.create(name:params[:name] ,ingredients:params[:ingredients],cook_time:params[:cook_time])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+  @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    # recipe.update(params[:recipe])
+    recipe.update(name:params[:name] ,ingredients:params[:ingredients],cook_time:params[:cook_time])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+     
+    recipe.destroy
+    redirect '/recipes'
+  end
   # code actions here!
 
 end
